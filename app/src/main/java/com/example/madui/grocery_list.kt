646cfcb92f23bd71198e70d1929d640111b86c63
MihisaraJayasinghe <pasanmihisarajayasinghe@gmail.com>
2023-03-17 17:43:44 +0530

    intial commit

---
+++ app/src/main/java/com/example/madui/grocery_list.kt
@@ -0,0 +1,11 @@
+package com.example.madui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class grocery_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grocery_list)
+    }
+}
