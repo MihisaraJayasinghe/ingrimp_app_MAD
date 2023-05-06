a515eafee5115036c0985cc82608d04c6b40d585
MihisaraJayasinghe <pasanmihisarajayasinghe@gmail.com>
2023-05-06 08:52:56 +0530

    color changed with some fixes

---
+++ app/src/main/java/com/example/missionmad2/activities/Login.kt
@@ -0,0 +1,12 @@
+package com.example.missionmad2.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.missionmad2.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
